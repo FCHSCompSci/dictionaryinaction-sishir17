1f7475ea503f85b44fdc485b062c86bf86a75641
Sishir Phuyal <sishir.phuyal@stu.jefferson.kyschools.us>
2018-09-18 08:32:30 -0400

    first commit

---
+++ catalog.py
@@ -0,0 +1,18 @@
+def make_book():
+    library_book = {
+	'title': title,
+	'author': author,
+	'genre': genre,
+	'checkout_date': checkout_date,
+	'checkout_by': firstname_lastname,
+	'return_by': return_date,
+
+    pass
+
+
+while True:
+    cmd = input("Do you want to check-in a book?")
+    if cmd == "y":
+        make_book()
+
+         
